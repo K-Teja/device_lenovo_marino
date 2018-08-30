e91884ed6a9a32c67ad7d06b505b4e8e02c52017
SivAs1234 <santhosesivan974@gmail.com>
2018-08-30 15:12:23 +0530

    update tree
    Signed-off-by: k-teja <tejachaitu96@gmail.com>

--- device.mk
+++ device.mk
@@ -1,1 +1,6 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc
